a4502adbdf39860fa0909847af80187df95cb0e4
sunvirgo <g_sun2java@163.com>
2020-09-08 00:43:28 +0800

    sunvirgo_hg:day01_Eureka

---
+++ eureka-customer/src/test/java/com/sunvirgo/eurekacustomer/EurekaCustomerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sunvirgo.eurekacustomer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaCustomerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ eureka-provider/src/main/java/com/sunvirgo/eurekaprovider/EurekaProviderApplication.java
@@ -0,0 +1,21 @@
+package com.sunvirgo.eurekaprovider;
+
+import com.netflix.appinfo.InstanceInfo;
+import com.netflix.discovery.EurekaClient;
+import org.apache.commons.lang.builder.ToStringBuilder;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+
+import java.util.List;
+
+@SpringBootApplication
+public class EurekaProviderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaProviderApplication.class, args);
+    }
+
+}
